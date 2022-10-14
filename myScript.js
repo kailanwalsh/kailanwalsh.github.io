5c1d089cc6cce02df6bd6fb1d3fe1ccc4f8baccc
kailanwalsh <kailanwalsh3@gmail.com>
2022-10-14 14:31:35 +0100

    UPDATE

--- myScript.js
+++ myScript.js
@@ -1,3 +1,13 @@
 $(window).scroll(function(){
     $(".sub").css("opacity", 1 - $(window).scrollTop() / 250);
   });
+
+
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
